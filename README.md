f06aaf784b8ecb13ef41eb3fef28919d15770f7a
Tomas Dvorak <dvtomas@gmail.com>
2019-08-08 09:23:34 +0200

    feat: In-app documentation, an attempt to see if a docs folder works in Github pages

--- README.md
+++ README.md
@@ -1,28 +1,9 @@
-# Slog Logs viewer
+# Portable LogS viewer
+
+**Go [here](here) to try it live and see the documentation.**
 
 Single-page JavaScript webapp for user-friendly viewing and filtering logs 
 in the [slog](https://crates.io/crates/slog) format. 
 Just open the page in your browser and you are ready to go.
 
 Written in the [Elm programming language](https://elm-lang.org/).
-
-Currently it expects the log lines to look like this:
-
-`Aug 04 12:58:20.509 INFO Connecting to simulated camera device, device: CAM01, DeviceManager: CAM01, module: KmsCompoundDevices`
-
-that is `MMM DD HH:MM:SS.MMM Severity Message`
-
-Multiline log messages are supported.
-
-Could probably be easily modified to support other formats.
-
-## TODO
-
- - **Parse arbitrary logs with customizable format**
- - User-definable severity levels
- - Improve performance for big logs
- - Remember filter settings
-
-## Known bugs
-
- - Only parses time component for computing time offsets and deltas. Those wIll wrap negative on midnight
--- src/Main.elm
+++ src/Main.elm
@@ -32,16 +32,46 @@ type alias Model = {
     filters: Filter.Model
     }
 
-helpText = """
-Welcome to the Log viewer.
-
-Use the control panel (F2) to open new log file,
-"""
+helpTexts = [
+    (Warn, "INTRO Welcome to the Portable Log viewer, TODO homepage"),
+    (Info, "INTRO This is a simple application to parse and browse modest-sized text logs. You can filter log messages and highlight particular substrings."),
+    (Info, "INTRO Right now the application can only parse logs in a very specific format produced by https://docs.rs/slog-term/2.4.1/slog_term/. Log entries look like this:"),
+    (Debug, "Apr 17 12:19:24.597 DEBG Listening, port: 12345, module: ProtoServer"),
+    (Info, "INTRO However, if you like the application, contact me, we can try to make it more generic. Support for more/customizable formats would be some work, but it's feasible for a motivated person. We could add support for JSON logs."),
+    (Warn, "FEATURES Simple, robust, portable, zero-install app running locally in your browser. No data is ever sent to any server. Save this page and open it later for an internet-less operation"),
+    (Info, "FEATURES Written exclusively in the Elm programming language for fun development experience"),
+    (Info, "FEATURES Various filters, optionally operating only in given context"),
+    (Info, "FEATURES Columns for cumulative time and delta time (difference to previous message time) for easy performance problems spotting"),
+    (Info, "FEATURES Highlight searched terms with different colors "),
+    (Warn, "USAGE Let's take a closer look on how to use the Portable Log Viewer"),
+    (Debug, "USAGE Use the control panel (F2) to load your own log file (try a real world example here: TODO) and to set-up filters."),
+    (Debug, "USAGE FILTERS There are currently three filters implemented: "),
+    (Debug, "USAGE FILTERS  - Accept Matching: a filter that only accepts entries containing a substring and rejects everything else"),
+    (Debug, "USAGE FILTERS  - Reject Matching: a filter that only accepts entries NOT containing a substring"),
+    (Debug, "USAGE FILTERS  - Level filter: Rejects everything with severity lower than selected severity"),
+    (Debug, "USAGE FILTERS Unique property of the filtering system is the filtering Context. When non-empty, the filters will only operate on entries from the given Context, and leave the rest untouched. Right now Context only means that the entry contains the context substring (try entering e.g. a context FILTERS or TODO to see how it works), but in the future context could be the class name for Java logs, key-value pairs for structured logs etc."),
+    (Debug, "USAGE FILTERS Filters can be also muted, i.e. disabled. That can be useful for temporarily inspecting hidden entries, or for using the Accept filter to highlight certain substrings."),
+    (Warn, "TODO Right now Portable Log Viewer is just a proof of concept. It works for me, but there's probably some work to be done should it be useful for others as well."),
+    (Info, "TODO ESSENTIAL Support for other log formats. The way to go would probably be JSON logs."),
+    (Info, "TODO ESSENTIAL Paging. The app chokes on logs even several thousand entries long. I think the bottleneck is simply the browser rendering the huge table."),
+    (Debug, "TODO HIGH_PRIORITY Case insensitive matching."),
+    (Debug, "TODO HIGH_PRIORITY Toggle highlighting for Accept filter. Add a 'Highlight only' filter type."),
+    (Debug, "TODO HIGH_PRIORITY Remember position and retain it when filter changes"),
+    (Trace, "TODO IDEA Unfolding filtered items"),
+    (Trace, "TODO IDEA User-selectable columns"),
+    (Trace, "TODO IDEA Regex support"),
+    (Trace, "TODO IDEA Fuzzy matching"),
+    (Trace, "TODO IDEA Remember recent logs, save them to local storage"),
+    (Trace, "TODO IDEA Remember filter settings, save them to local storage"),
+    (Warn, "BUGS Known bugs"),
+    (Error, "BUGS Delta time column displays delta to previous message, not previous DISPLAYED message"),
+    (Error, "BUGS Only parses time component for computing time offsets and deltas. Those wIll wrap negative on midnight")
+    ]
 
 init : () -> (Model, Cmd Msg)
 init _ = (
         {
-            log = [{ dateString = "now", timeString = "now", time = 0, cumulativeTime = 0, deltaTime = 0, severity = Info, text = helpText }],
+            log = List.map (\(severity, text) ->{ dateString = "now", timeString = "now", time = 0, cumulativeTime = 0, deltaTime = 0, severity = severity, text = text }) helpTexts,
             controlPanelMinimized = True,
             fileName = "",
             filters = Filter.emptyModel
